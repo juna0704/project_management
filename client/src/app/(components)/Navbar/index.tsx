f07c22f26e5427be4efbcd389ee11fca55cbbbbe
junaidalikhan <junaidalikhan0704@gmail.com>
2025-06-04 23:54:14 +0530

    added sidebar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { Search, Settings } from 'lucide-react';
-// import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { Menu, Moon, Search, Settings, Sun } from 'lucide-react';
+import { useAppDispatch, useAppSelector } from '@/app/redux';
 // import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
 import Link from 'next/link';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/state';
 
 const Navbar = () => {
-  //     const dispatch = useAppDispatch();
-  //   const isSidebarCollapsed = useAppSelector(
-  //     (state) => state.global.isSidebarCollapsed,
-  //   );
-  //   const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
 
   //   const { data: currentUser } = useGetAuthUserQuery({});
   //   const handleSignOut = async () => {
@@ -27,6 +28,13 @@ const Navbar = () => {
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search Bar */}
       <div className="flex items-center gap-8">
+        {!isSidebarCollapsed ? null : (
+          <button
+            onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}
+          >
+            <Menu className="h-8 w-8 dark:text-white" />
+          </button>
+        )}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -39,11 +47,26 @@ const Navbar = () => {
 
       {/* Settings icon */}
       <div className="flex items-center">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={
+            isDarkMode
+              ? `rounded p-2 dark:hover:bg-gray-700`
+              : `rounded p-2 hover:bg-gray-100`
+          }
+        >
+          {isDarkMode ? (
+            <Sun className="h-6 w-6 cursor-pointer dark:text-white" />
+          ) : (
+            <Moon className="h-6 w-6 cursor-pointer dark:text-white" />
+          )}
+        </button>
         <Link
           href="/settings"
           className={
-            `h-min w-min rounded p-2 dark:hover:bg-gray-700`
-            //   : `h-min w-min rounded p-2 hover:bg-gray-100`
+            isDarkMode
+              ? `h-min w-min rounded p-2 dark:hover:bg-gray-700`
+              : `h-min w-min rounded p-2 hover:bg-gray-100`
           }
         >
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
