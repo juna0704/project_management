68d01f769f164eb304907afbbe289193d3293596
junaidalikhan <junaidalikhan0704@gmail.com>
2025-06-04 05:49:58 +0530

    added navbar and sidebar

---
+++ client/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className="text-blue flex h-screen items-center justify-center bg-gray-900 text-black">
+      Home
+    </div>
+  );
+}
